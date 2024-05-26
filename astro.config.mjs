c63ef85e29a71b27bb0dd6f75600b8862bdbf901
YoharJB <yoharjb@gmail.com>
2024-05-25 21:24:17 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://yoharjb.github.io/",
+  site: "https://yoharjb.github.io",
   integrations: [
     sitemap(),
     robotsTxt({
